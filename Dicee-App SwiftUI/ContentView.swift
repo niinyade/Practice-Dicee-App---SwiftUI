5b1ff9c7206fd2900d61d666fa94524b2340e180
Nyade <niinyade@icloud.com>
2022-06-17 18:03:33 -0300

    Initial Commit

---
+++ Dicee-App SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Dicee-App SwiftUI
+//
+//  Created by Nyade on 17/06/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Dicee-App SwiftUI/Dicee_App_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_App_SwiftUIApp.swift
+//  Dicee-App SwiftUI
+//
+//  Created by Nyade on 17/06/22.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_App_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
